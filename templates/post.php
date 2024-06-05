b29f1557b18f862234464ba915d6c8df156bfbdb
MarcusMallia <marcusjohnmallia@gmail.com>
2024-06-05 05:44:08 +0200

    Fix issue with missing username key in post template and ensure proper data fetching

--- templates/post.php
+++ templates/post.php
@@ -22,7 +22,8 @@ $is_liked = $result_user_like->num_rows > 0;
 
 <div class="post">
     <h3><a href="/MarcusMallia-PHPsynoptic/scripts/post_details.php?post_id=<?php echo $post['post_id']; ?>"><?php echo htmlspecialchars($post['title']); ?></a></h3>
-    <p>By <a href="/MarcusMallia-PHPsynoptic/scripts/user_profile.php?user_id=<?php echo $post['user_id']; ?>"><?php echo htmlspecialchars($post['username']); ?></a></p>
+    <p>By <a href="/MarcusMallia-PHPsynoptic/scripts/user_profile.php?user_id=<?php echo $post['user_id']; ?>">
+        <?php echo isset($post['username']) ? htmlspecialchars($post['username']) : 'Unknown User'; ?></a></p>
     <p><?php echo htmlspecialchars($post['content']); ?></p>
     <?php if (!empty($post['link'])): ?>
         <p><a href="<?php echo htmlspecialchars($post['link']); ?>" target="_blank">Link</a></p>
